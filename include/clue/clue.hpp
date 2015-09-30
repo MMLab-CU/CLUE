5a72bdb71b80968067c72e719034d144b67a0009
Dahua Lin <lindahua@gmail.com>
2015-10-01 06:53:31 +0800

    add an include-all header

---
+++ include/clue/clue.hpp
@@ -0,0 +1,27 @@
+// inclusion of the entire CLUE++ library
+
+#ifndef CLUE_CLUE__
+#define CLUE_CLUE__
+
+#include <clue/config.hpp>
+
+// type traits & meta-programming
+#include <clue/type_traits.hpp>
+#include <clue/meta.hpp>
+#include <clue/meta_seq.hpp>
+
+// string and formatting
+#include <clue/string_view.hpp>
+#include <clue/stringex.hpp>
+#include <clue/formatting.hpp>
+
+// ranges and views
+#include <clue/value_range.hpp>
+#include <clue/array_view.hpp>
+#include <clue/reindexed_view.hpp>
+
+// other facilities
+#include <clue/optional.hpp>
+#include <clue/timing.hpp>
+
+#endif
---
+++ tests/test_include_all.cpp
@@ -0,0 +1,56 @@
+// to ensure <clue/clue.hpp> is correct
+
+#include <clue/clue.hpp>
+#include <clue/clue.hpp>   // ensure duplicated inclusion is ok
+
+// ensure that all headers are actually included
+
+// array_view
+using clue::array_view;
+
+// formatting
+using clue::fmt::with;
+using clue::fmt::str;
+
+// meta
+using clue::meta::type_;
+
+// meta_seq
+using clue::meta::seq_;
+
+// numformat
+using clue::fmt::int_formatter;
+using clue::fmt::float_formatter;
+
+// optional
+using clue::optional;
+
+// reindexed_view
+using clue::reindexed_view;
+
+// string_builder
+using clue::string_builder;
+
+// string_view
+using clue::string_view;
+
+// stringex
+using clue::trim;
+using clue::foreach_token_of;
+
+// timing
+using clue::stop_watch;
+using clue::calibrated_time;
+
+// type_traits
+using clue::enable_if_t;
+
+// value_range
+using clue::value_range;
+
+// string_view
+using clue::string_view;
+
+int main() {
+    return 0;
+}

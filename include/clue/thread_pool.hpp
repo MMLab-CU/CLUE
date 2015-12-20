9662edf7d7d6df401429f89c198bc97673088207
Dahua Lin <lindahua@gmail.com>
2015-12-20 08:38:10 +0800

    more member functions for thread pool + tested

--- include/clue/thread_pool.hpp
+++ include/clue/thread_pool.hpp
@@ -21,24 +21,26 @@ private:
         task_queue_t tsk_queue;
         std::condition_variable cv;
         std::mutex cv_mut;
-        std::atomic<size_t> n_waiting;
+        std::atomic<size_t> n_pushed;
+        std::atomic<size_t> n_completed;
         bool done;
-        bool terminated;
+        bool stopped;
 
         states_t()
-            : n_waiting(0)
+            : n_pushed(0)
+            , n_completed(0)
             , done(false)
-            , terminated(false) {}
+            , stopped(false) {}
     };
 
     struct entry_t{
         states_t& st;
         size_t idx;
-        bool terminated;
+        bool stopped;
         std::thread th;
 
         entry_t(states_t& s, size_t i)
-            : st(s), idx(i), terminated(false) {
+            : st(s), idx(i), stopped(false) {
             th = std::thread([this](){
                 task_func_t tfun;
                 bool got_tsk = st.tsk_queue.try_pop(tfun);
@@ -46,20 +48,19 @@ private:
                     // execute whatever remain in the queue
                     while(got_tsk) {
                         tfun(idx);
-                        if (terminated) return;
+                        ++ st.n_completed;
+                        if (stopped) return;
                         got_tsk = st.tsk_queue.try_pop(tfun);
                     }
-                    if (terminated || st.done) return;
+                    if (stopped || st.done) return;
 
                     // wait for notification
                     std::unique_lock<std::mutex> cv_lk(st.cv_mut);
-                    ++ st.n_waiting;
                     st.cv.wait(cv_lk, [&](){
-                        if (terminated) return true;
+                        if (stopped) return true;
                         got_tsk = st.tsk_queue.try_pop(tfun);
-                        return got_tsk || terminated || st.done;
+                        return got_tsk || stopped || st.done;
                     });
-                    -- st.n_waiting;
                 }
             });
         }
@@ -72,7 +73,7 @@ private:
 public:
     thread_pool() = default;
 
-    thread_pool(size_t nthreads) {
+    explicit thread_pool(size_t nthreads) {
         resize(nthreads);
     }
 
@@ -84,6 +85,31 @@ public:
         return entries_.size();
     }
 
+    const std::thread& get_thread(size_t idx) const {
+        return entries_.at(idx)->th;
+    }
+
+    std::thread& get_thread(size_t idx) {
+        return entries_.at(idx)->th;
+    }
+
+    size_t num_scheduled_tasks() const noexcept {
+        return states_.n_pushed.load();
+    }
+
+    size_t num_completed_tasks() const noexcept {
+        return states_.n_completed.load();
+    }
+
+    bool stopped() const noexcept {
+        return states_.stopped;
+    }
+
+    bool done() const noexcept {
+        return states_.done;
+    }
+
+public:
     void resize(size_t nthreads) {
         size_t n0 = size();
         if (nthreads > n0) {
@@ -94,6 +120,8 @@ public:
             for (size_t i = 0; i < na; ++i) {
                 entries_.emplace_back(new entry_t(states_, n0 + i));
             }
+            states_.stopped = false;
+            states_.done = false;
 
         } else if (nthreads < n0) {
             // reduce the thread pool
@@ -103,7 +131,7 @@ public:
             // terminate & detach threads
             for (size_t i = 0; i < nr; ++i) {
                 entry_t& e = *(entries_.back());
-                e.terminated = true;
+                e.stopped = true;
                 e.th.detach();
                 entries_.pop_back();
             }
@@ -111,29 +139,30 @@ public:
         }
     }
 
-    size_t idle_count() const noexcept {
-        return states_.n_waiting.load();
-    }
-
     template<class F>
     auto push(F&& f) -> std::future<decltype(f((size_t)0))> {
+        CLUE_ASSERT(!stopped() && !done());
+
         using pck_task_t = std::packaged_task<decltype(f((size_t)0))(size_t)>;
         auto sp = std::make_shared<pck_task_t>(std::forward<F>(f));
         states_.tsk_queue.push([sp](size_t idx){
             (*sp)(idx);
         });
+        states_.n_pushed ++;
         states_.cv.notify_one();
         return sp->get_future();
     }
 
     // block until all tasks finish
     void join() {
+        CLUE_ASSERT(!stopped() && !done());
+
         states_.done = true;
         states_.cv.notify_all();
 
         for (auto& pe: entries_) {
             if (pe->th.joinable()) pe->th.join();
-            pe->terminated = true;
+            pe->stopped = true;
         }
 
         // clear the threads
@@ -143,10 +172,12 @@ public:
 
     // block until all current tasks finish
     // remaining tasks are all cleared
-    void terminate() {
-        states_.terminated = true;
+    void stop() {
+        CLUE_ASSERT(!stopped() && !done());
+
+        states_.stopped = true;
         for (auto& pe: entries_) {
-            pe->terminated = true;
+            pe->stopped = true;
         }
         states_.cv.notify_all();
 
@@ -156,6 +187,7 @@ public:
         }
 
         // clear queue
+        states_.done = states_.tsk_queue.empty();
         states_.tsk_queue.clear();
 
         // clear the threads
---
+++ tests/test_thread_pool.cpp
@@ -0,0 +1,81 @@
+#include <clue/thread_pool.hpp>
+#include <cstdio>
+
+void test_construction() {
+    std::printf("TEST thread_pool: construction\n");
+    clue::thread_pool P;
+
+    assert(P.empty());
+    assert(0 == P.size());
+
+    P.resize(4);
+    assert(!P.empty());
+    assert(4 == P.size());
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    assert(!P.stopped());
+    assert(!P.done());
+
+    // verify that get_thread is ok
+    for (size_t i = 0; i < 4; ++i) P.get_thread(i);
+
+    P.join();
+
+    assert(0 == P.num_scheduled_tasks());
+    assert(0 == P.num_completed_tasks());
+    assert(!P.stopped());
+    assert(P.done());
+    assert(P.empty());
+}
+
+void task(size_t idx, size_t ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+void test_push_tasks_and_join() {
+    std::printf("TEST thread_pool: push tasks + join\n");
+    clue::thread_pool P(4);
+
+    assert(!P.empty());
+    assert(4 == P.size());
+
+    for (size_t i = 0; i < 20; ++i) {
+        P.push([](size_t tid){ task(tid, 5); });
+    }
+
+    P.join();
+
+    assert(20 == P.num_scheduled_tasks());
+    assert(20 == P.num_completed_tasks());
+    assert(!P.stopped());
+    assert(P.done());
+    assert(P.empty());
+}
+
+void test_early_stop() {
+    std::printf("TEST thread_pool: early stop\n");
+    clue::thread_pool P(2);
+
+    assert(!P.empty());
+    assert(2 == P.size());
+
+    for (size_t i = 0; i < 10; ++i) {
+        P.push([](size_t tid){ task(tid, 50); });
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(125));
+    P.stop();  // will wait for active tasks to finish
+
+    assert(10 == P.num_scheduled_tasks());    
+    assert(6 == P.num_completed_tasks());
+    assert(P.stopped());
+    assert(!P.done());
+    assert(P.empty());
+}
+
+int main() {
+    test_construction();
+    test_push_tasks_and_join();
+    test_early_stop();
+    return 0;
+}
